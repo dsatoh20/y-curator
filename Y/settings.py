199b7399c3ee83f7a951018944dfee29556bb930
dsatoh20 <dsatoh@GQ2DPW2>
2024-11-21 16:25:15 +0900

    fixed static settings

--- Y/settings.py
+++ Y/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [STATIC_DIR,]
 
 #MEDIA_DIR
 MEDIA_ROOT = MEDIA_DIR
