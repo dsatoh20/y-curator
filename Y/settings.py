7cae7735d7fa8837bae610e900c8edfc49f1cff4
dsatoh20 <dsatoh@GQ2DPW2>
2024-11-21 16:14:14 +0900

    edited code for read jawsdb

--- Y/settings.py
+++ Y/settings.py
@@ -96,20 +96,20 @@ WSGI_APPLICATION = 'Y.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if 'JAWSDB_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            env='JAWSDB_URL',
+            conn_max_age=600,
 
-if DEBUG:
+        )
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            env='JAWSDB_URL',
-            conn_max_age=600,
-        )
     }
 
 # Password validation
